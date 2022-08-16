5b79cc6164519237b5f570bf6a6f0b995ea420ab
Denis leibovich <leibovich2021@gmail.com>
2022-08-16 13:43:54 +0300

    file

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$ip_server = $_SERVER['SERVER_ADDR'];
+
+echo "<h1>Hello from kubernetes</h1><br>";
+echo "Sever IP Address is: $ip_server", "<br><p>";
+echo "Made by <font color=blue>Denis Leibovich";
+echo "VERSION1";
+?>
